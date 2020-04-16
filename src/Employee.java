84d692bfd0d10316e8749b0a17af5a6ac66aae99
wengdahl <wfengdahl@wpi.edu>
2020-04-16 19:00:34 -0400

    Sort in employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,7 +1,8 @@
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-    public abstract class Employee {
+public abstract class Employee {
         private ArrayList<Client> myClients;
 
         public Employee()
@@ -11,8 +12,18 @@ import java.util.ArrayList;
 
         public boolean addClient(Client c)
         {
-            //Temp, must sort
-            return myClients.add(c);
+            for(int i = 0; i<myClients.size();i++)
+            {
+                Client curr = myClients.get(i);
+                if(c.getClientID()>curr.getClientID())
+                {
+                    myClients.add(i,c);
+                    return true;
+                }else if(c.getClientID()==curr.getClientID()){
+                    return false;//ID duplicated, not possible
+                }
+            }
+            return myClients.add(c);//If not found, place at the end
         }
         private ArrayList<Client> getClients()
         {
