4b4be253117fa46fdf7758a45d179d8001fa7766
Eva <elabbe@wpi.edu>
2020-04-16 18:36:23 -0400

    client

---
+++ src/Client.java
@@ -0,0 +1,27 @@
+public class Client {
+
+    protected int getClientID() {
+        return clientID;
+    }
+
+    protected void setClientID(int clientID) {
+        this.clientID = clientID;
+    }
+
+    protected String getClientName() {
+        return clientName;
+    }
+
+    protected void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    private int clientID;
+    private String clientName;
+
+    double totalSales(){
+        return 0;
+    }
+
+
+}
