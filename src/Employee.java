9e0c87ef477b63d57702f9bc95b7178f30208902
wengdahl <wfengdahl@wpi.edu>
2020-04-18 11:33:15 -0400

    Add comments to employee and SalesManager classes

--- src/Employee.java
+++ src/Employee.java
@@ -1,15 +1,27 @@
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
+/**
+ * Employee class
+ * Abstract class representing a sales employee with assigned clients
+ */
 public abstract class Employee {
+    //Sorted list of clients
     private ArrayList<Client> myClients;
 
+    /**
+     * Constructor for employee
+     */
     public Employee()
     {
         myClients = new ArrayList<Client>();
     }
 
+    /**
+     * Add a client to this employee's list of clients in the correct location
+     * @param c the client to add
+     * @return true if successful, false if failed
+     */
     public boolean addClient(Client c)
     {
         for(int i = 0; i<myClients.size();i++)
@@ -20,22 +32,40 @@ public abstract class Employee {
                 myClients.add(i,c);
                 return true;
             }else if(c.getClientID()==curr.getClientID()){
-                return false;//ID duplicated, not possible
+                return false;//ID duplicated, not possible to have clients share an ID
             }
         }
         return myClients.add(c);//If not found, place at the end
     }
+
+    /**
+     * Get the list of clients
+     * @return the client arraylist
+     */
     private ArrayList<Client> getClients()
     {
         return myClients;
     }
 
-
+    /**
+     * Abstract method, to get name of employee
+     * @return employee name
+     */
     protected abstract String getEmployeeName();
+
+    /**
+     * Provide the number of clients in the list
+     * @return size of client arraylist
+     */
     protected int getNumClients()
     {
         return myClients.size();
     }
+
+    /**
+     * Get the string representation of the client list
+     * @return string including name and ID of all clients
+     */
     protected String clientString()
     {
         String s = " ";
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -2,40 +2,64 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
+/**
+ * Sales manager class
+ * An employee who is responsible for managing other employees
+ */
 public class SalesManager extends Employee{
+    //Name of the employee
     protected String employeeName;
+    //List of employees managed by this manager
     private ArrayList<Employee> employees;
+    //manager of this manager
     protected SalesManager manager;
+    //Employee ID number
     protected long salesID;
 
+    /**
+     * Constructor for salesmanager
+     * @param nm name of manager
+     * @param ID ID number of manager
+     */
     public  SalesManager(String nm, long ID)
     {
         employeeName = nm;
         salesID = ID;
         employees = new ArrayList<Employee>();
     }
+
+    /**
+     * Accessor for manager
+     * @return this employee's manager
+     */
     protected SalesManager getManager()
     {
         return manager;
     }
+
+    /**
+     * Setter for manager
+     * @param m the new manager of this employee
+     */
     protected void setManager(SalesManager m)
     {
         manager = m;
     }
 
-    private ArrayList getClients() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        //for accessing private method
-        Method m=Employee.class.getDeclaredMethod("getClients");
-        m.setAccessible(true);
-        return (ArrayList)(m.invoke(this.getClass().getSuperclass()));
-    }
-
-    protected double Bonus() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    /**
+     * Calculate and return the bonus of this manager
+     * @return 1000 * number of clients
+     */
+    protected double Bonus(){
         return 1000*super.getNumClients();
     }
 
-  public String toString()
-  {
+    /**
+     * To string method
+     * @return string including name, managed employees, and clients
+     */
+    public String toString()
+    {
       String s ="Sales  Manager: "+employeeName+". Employees: ";
         for (Employee e: employees)
         {
@@ -46,11 +70,18 @@ public class SalesManager extends Employee{
         return s;
     }
 
-    @Override
+    /**
+     * Get the name of this employee
+     * @return the employee's name
+     */
     protected String getEmployeeName() {
         return employeeName;
     }
 
+    /**
+     * Add an employee to the list of employees
+     * @param e the employee to add
+     */
     protected void addEmployee(Employee e)
     {
         employees.add(e);
