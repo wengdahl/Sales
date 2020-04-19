759522e63e0b13409e55744981df48b6a5445a01
Eva <elabbe@wpi.edu>
2020-04-18 23:38:46 -0400

    Finished

--- src/Client.java
+++ src/Client.java
@@ -1,47 +1,54 @@
 public class Client {
 
+    private int clientID;
+    private String clientName;
+    private Employee emp;
+
     public  Client(String nm, int ID)
     {
         clientName = nm;
         clientID = ID;
     }
 
+    /*
+    GETTERS----
+     */
     protected int getClientID() {
         return clientID;
     }
 
-    protected void setClientID(int clientID) {
-        this.clientID = clientID;
+    protected Employee getEmp() {
+        return emp;
     }
 
     protected String getClientName() {
         return clientName;
     }
 
-    protected void setClientName(String clientName) {
-        this.clientName = clientName;
+    protected String getLabelString()
+    {
+        return clientName+" "+clientID;
     }
 
-    private int clientID;
-    private String clientName;
+    /*
+    SETTERS-----
+     */
 
-    protected Employee getEmp() {
-        return emp;
+    protected void setClientID(int clientID) {
+        this.clientID = clientID;
+    }
+
+    protected void setClientName(String clientName) {
+        this.clientName = clientName;
     }
 
     protected void setEmp(Employee emp) {
         this.emp = emp;
     }
 
-    private Employee emp;
-
+    //start sales at 0
     double totalSales(){
         return 0;
-    }
-
-    protected String getLabelString()
-    {
-        return clientName+" "+clientID;
     }
 
     public String toString(){
--- src/SalesAssociate.java
+++ src/SalesAssociate.java
@@ -4,41 +4,50 @@ import java.util.ArrayList;
 
 public class SalesAssociate extends Employee {
 
+    protected SalesManager SalesManager;
+    protected long salesID;
+    protected String employeeName;
+
     public  SalesAssociate(String nm, long ID)
     {
         employeeName = nm;
         salesID = ID;
     }
 
-    protected String employeeName;
-
+    /*
+    Getters----
+     */
     protected String getEmployeeName() {
         return employeeName;
-    }
-
-    protected void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
     }
 
     protected SalesManager getSalesManager() {
         return SalesManager;
     }
 
-    protected void setSalesManager(SalesManager salesManager) {
-        SalesManager = salesManager;
-    }
-
     protected long getSalesID() {
         return salesID;
+    }
+
+    /*
+    SETTERS---
+     */
+
+    protected void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    protected void setSalesManager(SalesManager salesManager) {
+        SalesManager = salesManager;
     }
 
     protected void setSalesID(long salesID) {
         this.salesID = salesID;
     }
 
-    protected SalesManager SalesManager;
-    protected long salesID;
-
+    /*
+    make the bonus for an associate
+     */
     protected double Bonus() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         //for accessing private method
         Method m=Employee.class.getDeclaredMethod("getClients");
